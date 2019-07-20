45638ed32ccb9d1c94342ffd6b02426de036203f
Alexander Hjelm <alexander-hjelm@tutanota.com>
2019-07-20 20:59:46 +0200

    build html from run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,1 +1,2 @@
-~/godot/bin/godot_server.x11.opt.tools.32.llvm -d -s --path ~/godot_311_hook_test/ addons/gut/gut_cmdln.gd -gtest=res://test/unit/test_example.gd -glog=1 -gexit | python3 ~/godot-build-server-utils/parse-gut-logs.py ~/godot-build-server-utils/logs
+~/godot/bin/godot_server.x11.opt.tools.32.llvm -d -s --path ~/godot_311_hook_test/ addons/gut/gut_cmdln.gd -gtest=res://test/unit/test_example.gd -glog=0 -gexit | python3 ~/godot-build-server-utils/parse-gut-logs.py ~/godot-build-server-utils/logs;
+python3 ~/godot-build-server-utils/build-html.py ~/godot-build-server-utils/logs
